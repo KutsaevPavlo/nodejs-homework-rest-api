1374d5cde00ee4dd6e9aaf166922566f3f181f6e
Pavlo <p1kutsaev@gmail.com>
2023-06-03 13:26:48 +0300

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const app = require('./app')
 
 const {DB_HOST} = process.env;
 
-const app = require('./app')
-
 mongoose.set('strictQuery', true);
+
 mongoose.connect(DB_HOST)
 .then(()=> {app.listen(3000, () => {
   console.log("Database connection successful")
