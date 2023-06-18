7ba80f6a0c69e6c144851d4eed5a389ea4e60610
KutsaevPavlo <p1ul@ukr.net>
2023-06-18 12:29:36 +0300

    test

---
+++ routes/api/auth-routes.test.js
@@ -0,0 +1,34 @@
+const app = require("../../app");
+const moongoose = require("mongoose");
+const request = require('supertest');
+
+const {DB_HOST, PORT} = process.env;
+
+describe("test login route", ()=>{
+    let server = null;
+
+    beforeAll(async()=>{
+        await moongoose.connect(DB_HOST);
+        server = app.listen(PORT);
+    })
+
+    afterAll(async()=>{
+        await moongoose.connection.close();
+        server.close();
+    })
+
+    test("test correct login data", async()=>{
+        const loginData= {
+            email: "pavlo6@gmail.com",
+            password: "123456",
+        }
+        
+        const {body, statusCode, token} = await request(app).post("/users/login").send(loginData)
+        expect(statusCode).toBe(200)
+        expect(token).toBe()
+        expect(body.email).toBe(loginData.email)
+        expect(body.subscription).toBe()
+
+    })
+
+})
